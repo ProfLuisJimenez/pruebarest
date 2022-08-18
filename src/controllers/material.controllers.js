ba34338ea450b6e16bcfa66fc074df385954bf6c
github_ProfLuisJimenez <luis.jimenez@cutonala.udg.mx>
2022-08-18 16:42:35 -0500

    y tiene los metodos básicos

--- src/controllers/material.controllers.js
+++ src/controllers/material.controllers.js
@@ -12,9 +12,25 @@ const getmaterial= async(req,res)=>{
  }
  };
 
+ const getmat= async(req,res)=>{
+   try{
+      console.log(req.params);
+      const {id}=req.params;
+      const connection=await getConnection();
+      const result=await connection.query("SELECT id,nombre,descripcion,cantidad FROM material where id= ?", id);
+      res.json(result);
+   }catch(error){
+      res.status(500);
+      res.send(error.message);
+   }
+   };
+
  const addmaterial= async(req,res)=>{
     try{
        const {nombre,descripcion,cantidad}=req.body;
+       if(nombre == undefined || descripcion == undefined || cantidad == undefined){
+         res.status(400).json({message:"Por favor llena todos los campos"});
+       }
        const material={nombre,descripcion,cantidad};
        const connection=await getConnection();
        const result=await connection.query("INSERT INTO material SET ?",material);
@@ -25,7 +41,39 @@ res.json("agregar material");
     }
     };
 
+    const delmaterial= async(req,res)=>{
+      try{
+         const {id}=req.params;
+         const connection=await getConnection();
+         const result=await connection.query("DELETE FROM material WHERE id =?", id);
+         res.json(result);
+      }catch(error){
+         res.status(500);
+         res.send(error.message);
+      }
+      };
+
+      const updatematerial= async(req,res)=>{
+         try{
+            const {id}=req.params;
+            const {nombre,descripcion,cantidad}=req.body;
+            if(id==undefined || nombre == undefined || descripcion == undefined || cantidad == undefined){
+               res.status(400).json({message:"Por favor llena todos los campos"});
+             }
+            const material ={id,nombre,descripcion,cantidad};
+            const connection=await getConnection();
+            const result=await connection.query("UPDATE material SET =?", material);
+            res.json(result);
+         }catch(error){
+            res.status(500);
+            res.send(error.message);
+         }
+         };
+
 export const methods={
     getmaterial,
-    addmaterial
+    getmat,
+    addmaterial,
+    delmaterial,
+    updatematerial
 }
--- src/routes/material.routes.js
+++ src/routes/material.routes.js
@@ -4,6 +4,9 @@ import {methods as materialController} from "./../controllers/material.controlle
 const router=Router();
 
 router.get("/",materialController.getmaterial);
+router.get("/:id",materialController.getmat);
 router.post("/",materialController.addmaterial);
+router.delete("/:id",materialController.delmaterial);
+router.put("/:id",materialController.updatematerial);
 
 export default router;
